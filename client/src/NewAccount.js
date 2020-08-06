d7d8f4a1df5dd95b7cf675d5d498fe9eb0b4a7b6
Scyhthrine <rohangeniusity@gmail.com>
2020-08-06 19:31:13 +0530

    Improved Create An Account Section

--- client/src/NewAccount.js
+++ client/src/NewAccount.js
@@ -2,7 +2,9 @@ import React from "react";
 import Navbar from "./navbar";
 import Footer from "./footer";
 import Fade from "react-reveal/Fade";
-import { Container, Form, Button, Col } from "react-bootstrap";
+import { Container, Tabs, Tab } from "react-bootstrap";
+import NewMember from "./newMember";
+import ExistingMember from "./existingMember";
 
 export default function NewAccount() {
   return (
@@ -14,51 +16,18 @@ export default function NewAccount() {
       </header>{" "}
       <Container style={{ marginTop: "12em", marginBottom: "12em" }}>
         <Fade bottom>
-          <h1 style={{ textAlign: "center" }}>Become a Member</h1>
-          <Form>
-            <Form.Row>
-              <Form.Group as={Col} controlId="formGridEmail">
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-            </Form.Row>
-            <Form.Group controlId="formGridAddress1">
-              <Form.Label>Address</Form.Label>
-              <Form.Control placeholder="1234 Main St" />
-            </Form.Group>
-            <Form.Group controlId="formGridAddress2">
-              <Form.Label>Address 2</Form.Label>
-              <Form.Control placeholder="Apartment, studio, or floor" />
-            </Form.Group>
-            <Form.Row>
-              <Form.Group as={Col} controlId="formGridCity">
-                <Form.Label>City</Form.Label>
-                <Form.Control />
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridState">
-                <Form.Label>State</Form.Label>
-                <Form.Control as="select" defaultValue="Choose...">
-                  <option>Choose...</option>
-                  <option>...</option>
-                </Form.Control>
-              </Form.Group>
-
-              <Form.Group as={Col} controlId="formGridZip">
-                <Form.Label>Zip</Form.Label>
-                <Form.Control />
-              </Form.Group>
-            </Form.Row>
-
-            <Button variant="primary" type="submit">
-              Create an Account
-            </Button>
-          </Form>
+          <Tabs
+            defaultActiveKey="existingMember"
+            variant="pills"
+            id="uncontrolled-tab-example"
+          >
+            <Tab eventKey="newMember" title="New Member">
+              <NewMember />
+            </Tab>
+            <Tab eventKey="existingMember" title="Existing Member">
+              <ExistingMember />
+            </Tab>
+          </Tabs>
         </Fade>
       </Container>
       <footer>
