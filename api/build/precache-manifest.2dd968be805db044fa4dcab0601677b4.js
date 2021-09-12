78c20ef3a401c4a202403bc5ff993bc15ac70a14
Scyhthrine <rohangeniusity@gmail.com>
2021-09-12 09:14:25 +0530

    Build file generated

---
+++ api/build/precache-manifest.2dd968be805db044fa4dcab0601677b4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0ad55a9457a645901dbc45a82e020e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcb1b28743611541cca5",
+    "url": "/static/css/2.1c9ef103.chunk.css"
+  },
+  {
+    "revision": "fc6d614483ef117b0ebd",
+    "url": "/static/css/main.7787f3ae.chunk.css"
+  },
+  {
+    "revision": "fcb1b28743611541cca5",
+    "url": "/static/js/2.8f9d282b.chunk.js"
+  },
+  {
+    "revision": "fbdf34b30b710ad7b5359bcee244f523",
+    "url": "/static/js/2.8f9d282b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc6d614483ef117b0ebd",
+    "url": "/static/js/main.58510f85.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- api/build/service-worker.js
+++ api/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2a2527677bd108dfb0eb92de8eecbdab.js"
+  "/precache-manifest.2dd968be805db044fa4dcab0601677b4.js"
 );
 
 self.addEventListener('message', (event) => {
